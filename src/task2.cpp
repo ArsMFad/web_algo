66be48d1befc4cf7be51f26bb0ba30f74514563a
Arsenii <arsmfad@gmail.com>
2025-03-11 00:35:25 +0300

    added tests

--- src/task2.cpp
+++ src/task2.cpp
@@ -27,15 +27,31 @@ std::pair<int, int> exponentialSearch( int * array, int n )
     int left = i >> 1;
     int right = std::min( i, n - 1 );
 
+
     return { left, right };
 }
 
 
 void testExponentialSearch()
 {
-    int sampleCount = 100;
-    std::pair<int, int> expResult;
-    
+    {
+        int array[] = { 1, 3, 5, 7, 9, 8, 6, 4, 2 };
+        assert(exponentialSearch( array, 9 ) == std::make_pair( 4, 8 ));
+    }
+    {
+        int array[] = { 1, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        assert( exponentialSearch( array, 10 ) == std::make_pair( 1, 2 ) );
+    }
+    {
+        int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        assert( exponentialSearch( array, 19 ) == std::make_pair( 8, 16 ) );
+    }
+    {
+        int array[] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+        assert( exponentialSearch( array, 9 ) == std::make_pair( 4, 8 ) );
+    }
+
+    std::cout << "ALL EXPONENTIAL SEARCH TESTS DONE" << std::endl;
 }
 
 
@@ -53,6 +69,56 @@ int binarySearch( int * array, int left, int right )
     }
 
     return left;
+}
+
+
+void testBinarySearch()
+{
+    {
+        int array[] = { 1, 3, 5, 7, 9, 8, 6, 4, 2 };
+        assert( binarySearch( array, 4, 8 ) == 4 );
+    }
+    {
+        int array[] = { 1, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        assert( binarySearch( array, 1, 2 ) == 1 );
+    }
+    {
+        int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        assert( binarySearch( array, 8, 16 ) == 9 );
+    }
+    {
+        int array[] = { 1, 2, 3, 4, 5, 4, 3, 2, 1 };
+        assert( binarySearch( array, 4, 8 ) == 4 );
+    }
+
+    std::cout << "ALL BINARY SEARCH TESTS DONE" << std::endl;
+}
+
+
+void testLogic()
+{
+    {
+        int array[] = { 1, 3, 5, 7, 9, 8, 6, 4, 2 };
+        std::pair< int, int > borders = exponentialSearch( array, 9 );
+        assert(binarySearch( array, borders.first, borders.second ) == 4 );
+    }
+    {
+        int array[] = { 1, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        std::pair< int, int > borders = exponentialSearch( array, 10 );
+        assert( binarySearch( array, borders.first, borders.second ) == 1 );
+    }
+    {
+        int array[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+        std::pair< int, int > borders = exponentialSearch( array, 19 );
+        assert( binarySearch( array, borders.first, borders.second ) == 9 );
+    }
+    {
+        int array[] = {1, 2, 3, 4, 5, 4, 3, 2, 1};
+        std::pair< int, int > borders = exponentialSearch(array, 9);
+        assert( binarySearch( array, borders.first, borders.second ) == 4 );
+    }
+
+    std::cout << "ALL TESTS DONE" << std::endl;
 }
 
 
@@ -84,7 +150,7 @@ int main( int argc, const char *argv[] )
 {
     run( std::cin, std::cout );
     //testExponentialSearch();
-    //testBinaerySearch();
+    //testBinarySearch();
     //testLogic();
 
     return 0;
