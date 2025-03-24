c31c513f2b9313b55918d1402211bbc848893621
Arsenii <arsmfad@gmail.com>
2025-03-24 22:24:28 +0300

    now wokrs. test left

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 all: task4
 
-task3:
+task4:
 	g++ src/task4.cpp -o bin/task4
 
 clean:
--- src/task4.cpp
+++ src/task4.cpp
@@ -13,37 +13,22 @@ K отсортированных массивов суммарной длино�
 
 
 template <typename T>
-struct ArrayIterator
-{
-    T* buf;
-    int size;
-    int curPos;
-};
-
-
-template <typename T>
-bool comparator(const struct ArrayIterator &v1, const struct ArrayIterator &v2)
-{
-    return v1.buf[v1.curPos] < v2.buf[v2.curPos];
-}
-
-
-template <typename T>
 class Array
 {
 public:
     Array();
     ~Array();
-    Array(const Array& _arr);
 
-    T &operator[] ( int i );
+    T& operator[]( int i );
+    const T& operator[]( int i ) const;
+
     void Add( T element );
 
-    T Last();
+    T Last() const;
     void DeleteLast();
 
-    bool IsEmpty();
-    int Size();
+    bool IsEmpty() const;
+    int Size() const;
 private:
     T * buffer;
     int bufferSize;
@@ -53,12 +38,7 @@ private:
 };
 
 template <typename T>
-Array<T>::Array()
-{
-    buffer = new T[0];
-    bufferSize = 0;
-    actualSize = 0;
-}
+Array<T>::Array() : buffer(nullptr), bufferSize(0), actualSize(0) {}
 
 template <typename T>
 Array<T>::~Array()
@@ -67,26 +47,13 @@ Array<T>::~Array()
 }
 
 template <typename T>
-Array<T>::Array(const Array<T>& _arr)
+T& Array<T>::operator[]( int i )
 {
-    bufferSize = _arr.bufferSize;
-    actualSize = _arr.actualSize;
-
-    delete[] buffer;
-    buffer = new T[bufferSize];
-
-    for( int i = 0; i < actualSize; ++i )
-        arr[i] = _arr[i];
+    return buffer[i];
 }
 
 template <typename T>
-int Array<T>::Size()
-{
-    return bufferSize;
-}
-
-template <typename T>
-T & Array<T>::operator[] ( int i )
+const T& Array<T>::operator[]( int i ) const
 {
     return buffer[i];
 }
@@ -101,25 +68,7 @@ void Array<T>::Add( T element )
 }
 
 template <typename T>
-void Array<T>::upsize()
-{
-    bufferSize = (bufferSize == 0) ? 2 : bufferSize * 2;
-    T* newBuffer = new int [ bufferSize ];
-
-    std::copy( buffer, buffer + actualSize, newBuffer );
-
-    delete [] buffer;
-    buffer = newBuffer;
-}
-
-template <typename T>
-bool Array<T>::IsEmpty()
-{
-    return actualSize == 0;
-}
-
-template <typename T>
-T Array<T>::Last()
+T Array<T>::Last() const
 {
     return buffer[actualSize - 1];
 }
@@ -127,56 +76,104 @@ T Array<T>::Last()
 template <typename T>
 void Array<T>::DeleteLast()
 {
+    assert( actualSize > 0 );
     actualSize--;
+}
+
+template <typename T>
+int Array<T>::Size() const
+{
+    return actualSize;
+}
+
+template <typename T>
+bool Array<T>::IsEmpty() const
+{
+    return actualSize == 0;
+}
+
+template <typename T>
+void Array<T>::upsize()
+{
+    bufferSize = (bufferSize == 0) ? 2 : bufferSize * 2;
+    T* newBuffer = new T[ bufferSize ];
+
+    std::copy( buffer, buffer + actualSize, newBuffer );
+
+    delete [] buffer;
+    buffer = newBuffer;
 }
 
 
 template <typename T>
+struct ArrayIterator
+{
+    Array<T> *buffer;
+    int curPos;
+
+    T Value() const
+    {
+        return (*buffer)[curPos];
+    }
+
+    bool HasNext() const
+    {
+        return curPos + 1 < buffer->Size();
+    }
+
+    ArrayIterator Next() const
+    {
+        return {buffer, curPos + 1};
+    }
+};
+
+
+template <typename T, typename Comparator = std::less<T>>
 class Heap
 {
 public:
     Heap();
+    Heap(Comparator _comp);
     ~Heap();
-    explicit Heap( const Array<T>& _arr);
 
-    void Insert( T element );
+    void Insert( const ArrayIterator<T>& element );
 
-    int ExtractMax();
-    int PeekMax() const;
+    ArrayIterator<T> ExtractMin();
+    ArrayIterator<T> PeekMin() const;
+
+    bool IsEmpty() const;
 private:
-    Array<T> arr;
+    Array<ArrayIterator<T>> arr;
+    Comparator comp;
 
-    void buildHeap();
     void siftDown( int i );
     void siftUp( int i );
 };
 
 
-template <typename T>
-Heap<T>::Heap() {}
+template <typename T, typename Comparator>
+Heap<T, Comparator>::Heap() : comp(Comparator()) {}
 
-template <typename T>
-Heap<T>::~Heap() {}
+template <typename T, typename Comparator>
+Heap<T, Comparator>::Heap(Comparator _comp) : comp(_comp) {}
 
-template <typename T>
-Heap<T>::Heap( const Array<T>& _arr)
-{
-    arr = _arr;
-}
+template <typename T, typename Comparator>
+Heap<T, Comparator>::~Heap() {}
 
-template <typename T>
-void Heap<T>::Insert( T element )
+
+template <typename T, typename Comparator>
+void Heap<T, Comparator>::Insert( const ArrayIterator<T>& element )
 {
     arr.Add( element );
     siftUp( arr.Size() - 1 );
 }
 
-template <typename T>
-int Heap<T>::ExtractMax()
+template <typename T, typename Comparator>
+ArrayIterator<T> Heap<T, Comparator>::ExtractMin()
 {
     assert( !arr.IsEmpty() );
 
-    int result = arr[0];
+    ArrayIterator<T> result = arr[0];
     arr[0] = arr.Last();
     arr.DeleteLast();
 
@@ -186,69 +183,143 @@ int Heap<T>::ExtractMax()
     return result;
 }
 
-template <typename T>
-int Heap<T>::PeekMax() const
+template <typename T, typename Comparator>
+ArrayIterator<T> Heap<T, Comparator>::PeekMin() const
 {
     assert( !arr.IsEmpty() );
     return arr[0];
 }
 
-template <typename T>
-void Heap<T>::buildHeap()
-{
-    for( int i = arr.Size() / 2 - 1; i >= 0; --i )
-        siftDown(i);
+template <typename T, typename Comparator>
+bool Heap<T, Comparator>::IsEmpty() const {
+    return arr.IsEmpty();
 }
 
-template <typename T>
-void Heap<T>::siftDown( int i )
+template <typename T, typename Comparator>
+void Heap<T, Comparator>::siftDown( int i )
 {
     int left = 2 * i + 1;
     int right = 2 * i + 2;
 
-    int largest = i;
+    int smallest = i;
 
-    if (( left < arr.Size() ) && ( arr[left] > arr[i] ))
-        largest = left;
-    if (( right < arr.Size() ) && ( arr[right] > arr[largest] ))
-        largest = right;
+    if (( left < arr.Size() ) && comp( arr[ left ].Value(), arr[ smallest ].Value() ))
+        smallest = left;
+    if (( right < arr.Size() ) && comp( arr[ right ].Value(), arr[ smallest ].Value() ))
+        smallest = right;
     
-    if ( largest != i )
+    if ( smallest != i )
     {
-        std::swap( arr[i], arr[largest] );
-        siftDown( largest );
+        std::swap( arr[i], arr[smallest] );
+        siftDown( smallest );
     }
 }
 
-template <typename T>
-void Heap<T>::siftUp( int index )
+template <typename T, typename Comparator>
+void Heap<T, Comparator>::siftUp( int index )
 {
     while( index > 0 )
     {
         int parent = ( index - 1 ) / 2;
-        if( arr[index] <= arr[parent] )
-            return;
-        std::swap( arr[index], arr[parent] );
-        index = parent;
+        if( comp( arr[ index ].Value(), arr[ parent ].Value() ))
+        {
+            std::swap( arr[ index ], arr[ parent ] );
+            index = parent;
+        } else
+            break;
     }
+}
+
+
+template <typename T, typename Comparator>
+Array<T> mergeKSortedArrays(const Array<Array<T>*>& arrays, Comparator comp) {
+    Heap<T, Comparator> heap(comp);
+    Array<T> result;
+
+    for (int i = 0; i < arrays.Size(); ++i) {
+        if (!arrays[i]->IsEmpty()) {
+            ArrayIterator<T> iterator;
+            iterator.buffer = arrays[i];
+            iterator.curPos = 0;
+            heap.Insert(iterator);
+        }
+    }
+
+    while (!heap.IsEmpty()) {
+        ArrayIterator<T> minElement = heap.ExtractMin();
+        result.Add(minElement.Value());
+
+        if (minElement.HasNext()) {
+            heap.Insert(minElement.Next());
+        }
+    }
+
+    return result;
+}
+
+
+void run(std::istream& in, std::ostream& out)
+{
+    int K;
+    in >> K;
+
+    Array<Array<int>*> arrays;
+    while( K-- )
+    {
+        int size;
+        in >> size;
+
+        Array<int> *arr = new Array<int>();
+        for ( int j = 0; j < size; j++ )
+        {
+            int element;
+            in >> element;
+            arr->Add(element);
+        }
+        arrays.Add(arr);
+    }
+
+    Heap<int> heap;
+    for (int i = 0; i < arrays.Size(); i++)
+    {
+        if( !arrays[i]->IsEmpty() )
+        {
+            ArrayIterator<int> it;
+            it.buffer = arrays[i];
+            it.curPos = 0;
+            heap.Insert(it);
+        }
+    }
+
+    Array<int> result;
+
+    while( !heap.IsEmpty() )
+    {
+        ArrayIterator<int> min = heap.ExtractMin();
+        result.Add(min.Value());
+
+        if ( min.HasNext() )
+        {
+            heap.Insert( min.Next() );
+        }
+    }
+
+    for ( int i = 0; i < result.Size(); i++ )
+    {
+        out << result[i] << " ";
+    }
+    out << "\n";
+    
+
+    for( int i = 0; i < arrays.Size(); ++i )
+        delete arrays[i];
 }
 
 
 int main( int argc, const char *argv[] )
 {
-    Array<int> a1, a2, a3;
-    a1.Add(6);
-    a2.Add(50);
-    a2.Add(90);
-    a3.Add(1);
-    a3.Add(10);
-    a3.Add(70);
-
-    Heap<Array<int>> h;
-
-    h.Insert(a1);
-    h.Insert(a2);
-    h.Insert(a3);
+    run(std::cin, std::cout);
+    //testLogic();
     
     return 0;
 }
